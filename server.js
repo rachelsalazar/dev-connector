9a4fa9f349df3152e453c6dd3435f7a21278aed2
Rachel Salazar <ret1992@gmail.com>
2019-06-21 16:26:32 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //Db config
-const db = require('./config/keys').mongeURI;
+const db = require('./config/keys').mongoURI;
 mongoose.connect(db).then(() => console.log('MongoDb connected')).catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
